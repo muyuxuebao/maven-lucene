311c4683a45b9822b05f6dc4dc476be2514b77ee
yin,liang <muyuxuebao@163.com>
2015-09-16 14:56:32 +0800

    Change to the IDE named idea

--- src/main/java/com/yinliang/Lucene_3_5/analyzer/synonym/MySynonymAnalyzer.java
+++ src/main/java/com/yinliang/Lucene_3_5/analyzer/synonym/MySynonymAnalyzer.java
@@ -16,69 +16,69 @@ import com.chenlb.mmseg4j.Dictionary;
 import com.chenlb.mmseg4j.MaxWordSeg;
 import com.chenlb.mmseg4j.analysis.MMSegTokenizer;
 
-public class MySynonymAnalyzer extends Analyzer {
+public final class MySynonymAnalyzer extends Analyzer {
 
-	@Override
-	public TokenStream tokenStream(String fieldName, Reader reader) {
+    @Override
+    public TokenStream tokenStream(String fieldName, Reader reader) {
 
-		Dictionary dic = Dictionary.getInstance("data");
-		return new MySynonymFilter(new MMSegTokenizer(new MaxWordSeg(dic), reader));
-	}
+        Dictionary dic = Dictionary.getInstance("data");
+        return new MySynonymFilter(new MMSegTokenizer(new MaxWordSeg(dic), reader));
+    }
 
 }
 
-class MySynonymFilter extends TokenFilter {
-	CharTermAttribute charTermAttribute = null;
-	PositionIncrementAttribute positionIncrementAttribute = null;
+final class MySynonymFilter extends TokenFilter {
+    CharTermAttribute charTermAttribute = null;
+    PositionIncrementAttribute positionIncrementAttribute = null;
 
-	State currentState = null;
-	Stack<String> synonymStack = null;
+    State currentState = null;
+    Stack<String> synonymStack = null;
 
-	protected MySynonymFilter(TokenStream input) {
-		super(input);
-		this.charTermAttribute = this.addAttribute(CharTermAttribute.class);
-		this.positionIncrementAttribute = this.addAttribute(PositionIncrementAttribute.class);
+    protected MySynonymFilter(TokenStream input) {
+        super(input);
+        this.charTermAttribute = this.addAttribute(CharTermAttribute.class);
+        this.positionIncrementAttribute = this.addAttribute(PositionIncrementAttribute.class);
 
-		this.currentState = new State();
+        this.currentState = new State();
 
-		this.synonymStack = new Stack<String>();
-	}
+        this.synonymStack = new Stack<String>();
+    }
 
-	@Override
-	public boolean incrementToken() throws IOException {
-		if (this.synonymStack.size() > 0) {
-			// 将元素出栈,并获取这个元素
-			String str = this.synonymStack.pop();
+    @Override
+    public boolean incrementToken() throws IOException {
+        if (this.synonymStack.size() > 0) {
+            // 将元素出栈,并获取这个元素
+            String str = this.synonymStack.pop();
 
-			// 还原状态
-			this.restoreState(this.currentState);
-			this.charTermAttribute.setEmpty();
-			this.charTermAttribute.append(str);
+            // 还原状态
+            this.restoreState(this.currentState);
+            this.charTermAttribute.setEmpty();
+            this.charTermAttribute.append(str);
 
-			// 设置位置0
-			this.positionIncrementAttribute.setPositionIncrement(0);
-			return true;
-		}
+            // 设置位置0
+            this.positionIncrementAttribute.setPositionIncrement(0);
+            return true;
+        }
 
-		if (this.input.incrementToken() == false) {
-			return false;
-		}
+        if (this.input.incrementToken() == false) {
+            return false;
+        }
 
-		// 找同义词, 找到后放到栈(this.synonymStack)里面
-		String[] synonyms = this.getSynonyms(this.charTermAttribute.toString());
-		if (synonyms != null) {
-			this.currentState = this.captureState();
-			for (String string : synonyms) {
-				this.synonymStack.push(string);
-			}
-		}
-		return true;
-	}
+        // 找同义词, 找到后放到栈(this.synonymStack)里面
+        String[] synonyms = this.getSynonyms(this.charTermAttribute.toString());
+        if (synonyms != null) {
+            this.currentState = this.captureState();
+            for (String string : synonyms) {
+                this.synonymStack.push(string);
+            }
+        }
+        return true;
+    }
 
-	private String[] getSynonyms(String name) {
-		Map<String, String[]> map = new HashMap<String, String[]>();
-		map.put("我", new String[] { "俺", "咱" });
-		map.put("中国", new String[] { "天朝", "中华人民共和国" });
-		return map.get(name);
-	}
+    private String[] getSynonyms(String name) {
+        Map<String, String[]> map = new HashMap<String, String[]>();
+        map.put("我", new String[]{"俺", "咱"});
+        map.put("中国", new String[]{"天朝", "中华人民共和国"});
+        return map.get(name);
+    }
 }
--- src/test/java/com/yinliang/Lucene_3_5/test/MySynonymAnalyzerTest.java
+++ src/test/java/com/yinliang/Lucene_3_5/test/MySynonymAnalyzerTest.java
@@ -23,44 +23,53 @@ import com.yinliang.Lucene_3_5.analyzer.synonym.MySynonymAnalyzer;
 
 public class MySynonymAnalyzerTest {
 
-	@Test
-	public void test() {
-		try {
-			AnalyzerUtil analyzerUtil = new AnalyzerUtil();
-			String txt = "这是我的家, 我来自中国安徽省六安市苍墩村";
+    @Test
+    public void test() {
+        try {
+            AnalyzerUtil analyzerUtil = new AnalyzerUtil();
+            String txt = "这是我的家, 我来自中国安徽省六安市苍墩村";
 
-			Directory directory = new RAMDirectory();
+            Directory directory = new RAMDirectory();
 
-			Document doc = new Document();
-			doc.add(new Field("content", txt, Field.Store.YES, Field.Index.ANALYZED));
+            Document doc = new Document();
+            doc.add(new Field("content", txt, Field.Store.YES, Field.Index.ANALYZED));
 
-			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35, new MySynonymAnalyzer()));
-			indexWriter.addDocument(doc);
-			indexWriter.close();
+            IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35, new MySynonymAnalyzer()));
+            indexWriter.addDocument(doc);
+            indexWriter.close();
 
-			IndexSearcher indexSearcher = new IndexSearcher(IndexReader.open(directory));
+            IndexSearcher indexSearcher = new IndexSearcher(IndexReader.open(directory));
 
-			// 因为同义词的关系, 以下都可以搜到 content 为 "这是我的家, 我来自中国安徽省六安市苍墩村"的 Document
-			TopDocs tds = indexSearcher.search(new TermQuery(new Term("content", "我")), 10);
-			TopDocs tds1 = indexSearcher.search(new TermQuery(new Term("content", "咱")), 10);
-			TopDocs tds2 = indexSearcher.search(new TermQuery(new Term("content", "俺")), 10);
-			TopDocs tds3 = indexSearcher.search(new TermQuery(new Term("content", "中国")), 10);
-			TopDocs tds4 = indexSearcher.search(new TermQuery(new Term("content", "天朝")), 10);
-			TopDocs tds5 = indexSearcher.search(new TermQuery(new Term("content", "中华人民共和国")), 10);
+            // 因为同义词的关系, 以下都可以搜到 content 为 "这是我的家, 我来自中国安徽省六安市苍墩村"的 Document
+            TopDocs tds = indexSearcher.search(new TermQuery(new Term("content", "我")), 10);
+            TopDocs tds1 = indexSearcher.search(new TermQuery(new Term("content", "咱")), 10);
+            TopDocs tds2 = indexSearcher.search(new TermQuery(new Term("content", "俺")), 10);
+            TopDocs tds3 = indexSearcher.search(new TermQuery(new Term("content", "中国")), 10);
+            TopDocs tds4 = indexSearcher.search(new TermQuery(new Term("content", "天朝")), 10);
+            TopDocs tds5 = indexSearcher.search(new TermQuery(new Term("content", "中华人民共和国")), 10);
 
-			Document d = indexSearcher.doc(tds5.scoreDocs[0].doc);
-			System.out.println(d.get("content"));
+            Document d = indexSearcher.doc(tds4.scoreDocs[0].doc);
+            System.out.println(d.get("content"));
 
-			// analyzerUtil.dispalyAllTokenInfo(txt, new MySynonymAnalyzer());
-		} catch (CorruptIndexException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (LockObtainFailedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            // analyzerUtil.dispalyAllTokenInfo(txt, new MySynonymAnalyzer());
+        } catch (CorruptIndexException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (LockObtainFailedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void test1() {
+        AnalyzerUtil analyzerUtil = new AnalyzerUtil();
+        String txt = "这是我的家, 我来自中国安徽省六安市苍墩村";
+        analyzerUtil.dispalyAllTokenInfo(txt, new MySynonymAnalyzer());
+    }
+
 }
